508102f38930963b6cc79d01837fc78697d22df7
dev <jiecnik05@ya.ru>
2024-11-02 16:33:42 +0700

    Services package complit

---
+++ src/services/Cipher.java
@@ -0,0 +1,34 @@
+package services;
+
+import utilits.Alphabet;
+
+public class Cipher {
+    public static char shiftCharacter(char c, int key, char[] alphabet) {
+        int index = -1;
+        for (int i = 0; i < alphabet.length; i++) {
+            if (c == alphabet[i]) {
+                index = i;
+                break;
+            }
+        }
+
+    if(index !=-1) {
+        int newIndex = (index + key) % alphabet.length;
+        if (newIndex < 0) {
+            newIndex += alphabet.length;
+        }
+        return alphabet[newIndex];
+    }
+    return c;
+}
+
+public static String caesarCiper(String text, int key) {
+    StringBuilder result = new StringBuilder();
+    for(char c : text.toCharArray()){
+        result.append(Cipher.shiftCharacter(c, key, Alphabet.ALPHABET_RUS));
+    }
+    return result.toString();
+}
+}
+
+
